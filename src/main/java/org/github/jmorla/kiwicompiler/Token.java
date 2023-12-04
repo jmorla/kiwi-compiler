f1f5d292d49d2db9fdcead1c115d6f25783a50a7
Jorge Morla <jorgemorlapro@gmail.com>
2023-12-04 15:32:58 -0400

    small token/tokenType improvements

--- src/main/java/org/github/jmorla/kiwicompiler/Token.java
+++ src/main/java/org/github/jmorla/kiwicompiler/Token.java
@@ -1,13 +1,3 @@
 package org.github.jmorla.kiwicompiler;
 
-public record Token(TokenType type, String lexeme, int line, int column) {
-
-    @Override
-    public String toString() {
-        return "{" +
-                "type=" + type +
-                ", lexeme='" + lexeme + '\'' +
-                ", line=" + line +
-                '}';
-    }
-}
+public record Token(TokenType type, String lexeme, int line, int column) {}
--- src/main/java/org/github/jmorla/kiwicompiler/TokenType.java
+++ src/main/java/org/github/jmorla/kiwicompiler/TokenType.java
@@ -12,5 +12,6 @@ public enum TokenType {
     //keywords
     IMPORT, RENDER,
     // end of file
-    EOF
+    EOF,
+    NONE
 }
